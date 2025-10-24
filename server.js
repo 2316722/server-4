afc29a9f341ed411e409078c26816e85d24bc5a5
2316722 <206804375+2316722@users.noreply.github.com>
2025-10-24 12:11:07 +0900

    変更

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+app.get('api', (c) => {
+  return c.json({ message: 'Hello!!' });
+});
+
+Deno.serve(app.fetch);
