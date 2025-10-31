84f7f3cb0959f2c10fd2ed93628c149a2293b6a8
2316722 <206804375+2316722@users.noreply.github.com>
2025-10-31 12:08:15 +0900

    更新

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+app.get('/api', async (c) => {
+  return c.json({ massage: 'GET' });
+});
+
+Deno.serve(app.fetch);
